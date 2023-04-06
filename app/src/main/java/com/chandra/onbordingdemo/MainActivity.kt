bb433c1c609f68586c62230b250f19182151026e
Chandra Sekhar <sekhar.chandra.5832@gmail.com>
2023-04-06 23:51:04 +0530

    necessary items added

--- app/src/main/java/com/chandra/onbordingdemo/MainActivity.kt
+++ app/src/main/java/com/chandra/onbordingdemo/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.chandra.onbordingdemo
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
     }
 }
