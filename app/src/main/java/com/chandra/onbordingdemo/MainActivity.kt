8a0f3e8a587ad09f5f3a209600a717712dc0e4ad
Chandra Sekhar <sekhar.chandra.5832@gmail.com>
2023-04-08 23:15:56 +0530

    SplashScreen added

--- app/src/main/java/com/chandra/onbordingdemo/MainActivity.kt
+++ app/src/main/java/com/chandra/onbordingdemo/MainActivity.kt
@@ -1,47 +1,11 @@
 package com.chandra.onbordingdemo
 
-import android.content.Intent
-import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager2.widget.ViewPager2
-import com.tbuonomo.viewpagerdotsindicator.DotsIndicator
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var adapter: ViewPagerAdapter
-    private lateinit var viewPager2: ViewPager2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportActionBar?.hide()
-        viewPager2 = findViewById(R.id.viewPager2)
-        adapter = ViewPagerAdapter()
-        viewPager2.adapter = adapter
-        val springDotsIndicator = findViewById<DotsIndicator>(R.id.spring_dots_indicator)
-        springDotsIndicator.attachTo(viewPager2)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        findViewById<TextView>(R.id.txt_next).setOnClickListener {
-            if (viewPager2.currentItem == viewPager2.adapter?.itemCount?.minus(1)) {
-               welcomeScreen()
-            }
-            viewPager2.setCurrentItem(viewPager2.currentItem + 1, true)
-        }
-        findViewById<TextView>(R.id.txt_skip).setOnClickListener {
-            welcomeScreen()
-        }
-
-        adapter.onContinueButtonClicked = {
-            welcomeScreen()
-            finish()
-        }
-    }
-
-    private fun welcomeScreen() {
-        startActivity(Intent(this, WelcomeActivity::class.java))
     }
 }
